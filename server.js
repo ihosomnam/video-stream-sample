dcae49c65c851da74382e5a33510fef21d84f54e
Iho Somnam <ihosomnam4741@gmail.com>
2021-02-26 02:23:16 +0700

    add new video

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
 })
 
 app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4'
+  const path = 'assets/video.mp4'
   const stat = fs.statSync(path)
   const fileSize = stat.size
   const range = req.headers.range
